3dbbe1ee7386bbce99547ec80b81a041f54abe8f
Samuel Bancroft <samuelbancroft@hotmail.com>
2018-12-20 16:52:43 +0000

    added encryption for conn string.

---
+++ FeedbackAppV1.0/EncryptCS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FeedbackAppV1._0
+{
+    class EncryptCS
+    {
+        public static void EncryptConnectionString(bool encrypt, string fileName)
+        {
+            Configuration configuration = null;
+            try
+            {
+                configuration = ConfigurationManager.OpenExeConfiguration(fileName);
+                ConnectionStringsSection configSection = configuration.GetSection("connectionStrings") as ConnectionStringsSection;
+                if ((!(configSection.ElementInformation.IsLocked)) && (!(configSection.SectionInformation.IsLocked)))
+                {
+                    if (encrypt && !configSection.SectionInformation.IsProtected)
+                    {
+                        configSection.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+                    }
+
+                    if (!encrypt && configSection.SectionInformation.IsProtected)
+                    {
+                        configSection.SectionInformation.UnprotectSection();
+                    }
+
+                    configSection.SectionInformation.ForceSave = true;
+                    configuration.Save();
+                    Process.Start("notepad.exe", configuration.FilePath);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
