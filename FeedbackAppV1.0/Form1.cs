0fdfdda03118be57c6bcb866b44d154cb034839f
Samuel Bancroft <samuelbancroft@hotmail.com>
2018-12-20 15:19:58 +0000

    Refactored

--- FeedbackAppV1.0/Form1.cs
+++ FeedbackAppV1.0/Form1.cs
@@ -13,8 +13,8 @@ namespace FeedbackAppV1._0
 
         private void Form1_Load(object sender, System.EventArgs e)
         {
-            var connectionString = "Data Source=mssql2008R2.aspnethosting.co.uk,14330; Initial Catalog=samuelba_feedback; User ID=samuelba_admin; Password=sl71064LS";
-            SqlConnection conn = new SqlConnection(connectionString);
+            const string connectionString = @"Data Source=mssql2008R2.aspnethosting.co.uk,14330; Initial Catalog=samuelba_feedback; User ID=samuelba_admin; Password=sl71064LS";
+            var conn = new SqlConnection(connectionString);
             try
             {
                 conn.Open();
